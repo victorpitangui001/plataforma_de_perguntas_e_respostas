e4181f6c757e5570a51e0245f3dec448e5c67bd5
Victorpitangui001 <pitanguigit@gmail.com>
2023-12-18 22:21:11 -0300

    instalação do Sequelize e do Myql2

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/perguntar", (req,res) => {
+app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao
+    res.send(`Formulario recebido ! titulo: ${titulo} descriao: ${descricao}`)
 })
 
 app.listen(3000, () => {
